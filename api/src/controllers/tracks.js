6edc7adf10cd6707cb8aa65998bd000d6e6e1b97
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-06-15 17:45:01 -0300

    Ending CRUD tracks

--- api/src/controllers/tracks.js
+++ api/src/controllers/tracks.js
@@ -2,6 +2,16 @@ const bucket = require('../storage.js');
 const {STORAGE_BASEURL} = process.env;
 const db = require('../firebase.js').database();
 
+const getAllTracks = (req, res, next) => {
+	db.ref('tracks')
+		.once('value')
+		.then((snapshot) => {
+			const tracks = snapshot.val();
+			res.send({response: tracks, message: 'Successful'});
+		})
+		.catch((error) => next(error));
+};
+
 const uploadTrack = async (req, res, next) => {
 	//req.body = {...JSON.parse(req.body.body)};
 	const {name, year, serie} = req.body;
@@ -26,13 +36,60 @@ const uploadTrack = async (req, res, next) => {
 			audioUrl,
 			imageUrl,
 		};
-		db.ref('tracks').push(newTrack);
+		await db.ref('tracks').push(newTrack);
 		res.send({response: newTrack, message: 'Successful'});
 	} catch (error) {
 		next(error);
 	}
 };
 
+const updateTrack = async (req, res, next) => {
+	const id = req.params.id;
+	if (!id)
+		return res.status(400).send({
+			response: '',
+			message: 'Failed',
+		});
+	const {name, year, serie, audioUrl, imageUrl} = req.body;
+	const files = req.files;
+
+	try {
+		if (files) {
+			const data = await Promise.all(
+				files.map((file) =>
+					bucket.upload(file.path, {destination: file.filename})
+				)
+			);
+			audioUrl = STORAGE_BASEURL + data[0][0].name;
+			imageUrl = STORAGE_BASEURL + data[1][0].name;
+		}
+		await db
+			.ref('tracks/' + id)
+			.update({name, year, serie, audioUrl, imageUrl});
+		res.send({response: 'Updated', message: 'Successful'});
+	} catch (error) {
+		next(error);
+	}
+};
+
+const deleteTrack = async (req, res, next) => {
+	const id = req.params.id;
+	if (!id)
+		return res.status(400).send({
+			response: '',
+			message: 'Failed',
+		});
+	try {
+		await db.ref('tracks/' + id).remove();
+		res.send({response: 'Deleted', message: 'Successful'});
+	} catch (error) {
+		next(error);
+	}
+};
+
 module.exports = {
+	getAllTracks,
 	uploadTrack,
+	updateTrack,
+	deleteTrack,
 };
