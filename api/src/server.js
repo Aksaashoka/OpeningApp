48d6859c07b22f827abe750a2bec9e7c9d57d378
Camila Alvarez <alvarez.camila.alexandra@gmail.com>
2021-06-14 21:08:57 -0300

    Starting server

---
+++ api/src/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const cors = require('cors');
+const routes = require('./routes/index.js');
+
+server.use(express.json());
+server.use(morgan('dev'));
+server.use(
+	cors({
+		origin: '*',
+		credentials: true,
+		methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'DELETE'],
+		allowedHeaders: [
+			'Origin',
+			'X-Requested-With',
+			'Content-Type',
+			'Accept',
+			'authorization',
+		],
+	})
+);
+
+server.use('/', routes);
+
+module.exports = server;
